98bdfb1aed53252e67e453917dc0b4668ac8b906
近藤整 <hitoshi@kondouseinoAir.AirPort>
2021-06-07 11:05:25 +0900

    first commit

---
+++ lib/simplecrawler.ex
@@ -0,0 +1,18 @@
+defmodule Simplecrawler do
+  @moduledoc """
+  Documentation for `Simplecrawler`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Simplecrawler.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/simplecrawler_test.exs
@@ -0,0 +1,8 @@
+defmodule SimplecrawlerTest do
+  use ExUnit.Case
+  doctest Simplecrawler
+
+  test "greets the world" do
+    assert Simplecrawler.hello() == :world
+  end
+end
